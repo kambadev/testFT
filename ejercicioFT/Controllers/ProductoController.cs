c0e8757b6e40a60fcfa757e05dab7095ea20a0e3
Fabio <ar.fabiomolina@gmail.com>
2020-01-15 21:43:41 -0300

    Creadas rutas y vista de Listado (Productos/) y detalle (Producto/{id})

---
+++ ejercicioFT/Controllers/ProductoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ejercicioFT.Models;
+using ejercicioFT.Data;
+
+namespace ejercicioFT.Controllers
+{
+    public class ProductoController : Controller
+    {
+        ProductosRepo _productosRepo = new ProductosRepo();
+        // GET: Lista
+        [Route("Productos/")]
+        public ActionResult Listado()
+        {
+            return View(_productosRepo);
+
+        }
+
+        [Route("Producto/{id}")]
+        public ActionResult Detalle(string id)
+        {
+
+            return View(_productosRepo.ObtenerPorId(int.Parse(id)));
+
+        }
+
+        /*public void Index() {
+            Response.Write("<h1>Aca se mostrará la lista de productos</h1>");
+        }*/
+    }
+}
